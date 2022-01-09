57f04a1ef3e096cc0e630846c13ce0aaadc32188
wutchara <sstmuham@gmail.com>
2022-01-09 23:00:21 +0700

    Initialize project and add code for solve the issue

---
+++ README.md
@@ -0,0 +1,25 @@
+# Challenge-21-BIG-O-Question
+
+---
+
+
+```
+Given a array of X number of strings, find the longest common prefix among all strings present in the array.
+
+Input:
+An array of different items. For example: ['pea', 'pear', 'apple', 'for', 'april', 'apendix', 'peace', 1]
+
+Output:
+Print the longest common prefix as a string in the given array. If no such prefix exists print "-1"(without quotes).
+
+
+Example:
+Input:
+['pea', 'pear', 'apple', 'for', 'april', 'apendix', 'peace', 1]
+
+Output:
+['pea', 'ap']
+
+Explanation:
+Longest common prefix in all the given strings is 'pea'.
+```
---
+++ sol1.js
@@ -0,0 +1,109 @@
+const inputs = ['pea', 'pear', 'apple', 'for', 'april', 'apendix', 'peace', 1];
+console.log('inputs', inputs);
+
+// find maximum prefix
+const prefixMax = {};
+for (const input of inputs) {
+    if (typeof input === 'string') {
+        if (!prefixMax[input[0]]) {
+            prefixMax[input[0]] = {};
+        }
+        let current = prefixMax[input[0]];
+        for(const char of input.slice(1)) {
+            if (!current[char]) {
+                current[char] = {};
+            }
+            current = current[char];
+        }
+    }
+}
+// console.log('prefixMax', JSON.stringify(prefixMax));
+
+const prefix_popular = {
+    list_item_count: 0,
+    list: [],// { }[]
+};
+const prefix_count = JSON.parse(JSON.stringify(prefixMax)); // copy
+for (const i in inputs) {
+    const input = inputs[i];
+    if (typeof input === 'string') {
+        let current = prefix_count[input[0]];
+        let char_com = input[0];
+        for(const char of input.slice(1)) {
+            char_com += char;
+            const text_length = char_com.length;
+            if (current[char].index) {
+                current[char].index.push(i);
+                const current_item_length = current[char].index.length;
+                if (current_item_length > prefix_popular.list_item_count) {
+                    prefix_popular.list_item_count = current_item_length;
+                    prefix_popular.list = [{
+                        items_index: current[char].index.map(i => +i),
+                        text: char_com,
+                    }];
+                } else if (current_item_length === prefix_popular.list_item_count) {
+                    prefix_popular.list.push({
+                        items_index: current[char].index.map(i => +i),
+                        text: char_com,
+                    });
+                }
+            } else {
+                current[char].index = [i];
+            }
+            current = current[char];
+        }
+    }
+}
+// console.log('prefix_count', JSON.stringify(prefix_count));
+// console.log('prefix_popular', JSON.stringify(prefix_popular));
+
+const prefix_final = [];
+if (prefix_popular.list.length > 0) {
+    // group by 'items_index'
+    const group = {};
+    for (const i in prefix_popular.list) {
+        const p = prefix_popular.list[i];
+        const key = JSON.stringify(p.items_index);
+
+        if (group[key]) {
+            group[key].push({
+                prefix: p,
+                index: i,
+                text: p.text,
+            });
+        } else {
+            group[key] = [{
+                prefix: p,
+                index: i,
+                text: p.text,
+            }];
+        }
+    }
+
+    Object.values(group).map(g => {
+        if (g.length === 1) {
+            prefix_final.push(g[0].prefix);
+        } else {
+            // more than 1
+            // find longest text
+            let long_text = g[0].text;
+            for (const in_group of g) {
+                if (in_group.text.length > long_text.length) {
+                    long_text = in_group.text;
+                }
+            }
+            prefix_final.push(g.find(i => i.text === long_text).prefix);
+        }
+    });
+}
+// console.log('prefix_final', JSON.stringify(prefix_final));
+
+console.log('===================================================');
+console.log('========= Print the longest common prefix =========');
+console.log('===================================================');
+prefix_final.map(prefix => {
+    console.log(`Text: '${prefix.text}' ===> ${prefix.items_index.map(index => {
+        return inputs[index];
+    }).join(',')}`);
+});
+console.log('===================================================');
